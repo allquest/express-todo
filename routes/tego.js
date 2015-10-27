f7f972beff4cd9c7b980ff4e6b9823d61f8e9471
allquest <all_quest@hotmail.fr>
2015-10-27 21:59:46 +0100

    Added a page "tego" to test

--- routes/tego.js
+++ routes/tego.js
@@ -1,3 +1,12 @@
 /**
  * Created by Tegomass on 27/10/2015.
  */
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/tego', function (req, res, next) {
+    res.render('tego.jade', {title: 'TegoPage'});
+});
+
+module.exports = router;
